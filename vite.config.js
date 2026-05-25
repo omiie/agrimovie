6af90d5c23e9f89f2fa22c768ccb7ddccf38b6b4
inkAja <mdghifari17@gmail.com>
2026-05-25 09:11:20 +0700

    test perbaikan

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/', // Tambahkan baris ini agar path asset tidak berantakan di Netlify
-  // konfigurasi lainnya...
+  base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        eventProgram: resolve(__dirname, 'eventProgram.html'),
+        eventUpcoming: resolve(__dirname, 'eventUpcoming.html'),
+        filmAchievement: resolve(__dirname, 'filmAchievement.html'),
+        filmAll: resolve(__dirname, 'filmAll.html'),
+        filmBts: resolve(__dirname, 'filmBts.html'),
+      },
+    },
+  },
 })
